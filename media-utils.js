6803e621562db5a9de825f5b596cfd635eacb851
Daremon <163944235+RudyKotJeKoc@users.noreply.github.com>
2025-10-18 11:51:22 +0200

    fix: ignore aborted playback errors (#33)

--- media-utils.js
+++ media-utils.js
@@ -40,3 +40,36 @@ export function waitForMediaReady(mediaElement, { timeout = 5000 } = {}) {
         }
     });
 }
+
+const INTERRUPTED_MESSAGES = [
+    'The play() request was interrupted by a call to pause()',
+    'The play() request was interrupted by a new load request'
+];
+
+export function shouldIgnorePlaybackError(error) {
+    if (!error) {
+        return false;
+    }
+
+    const name = typeof error === 'object' && error !== null && 'name' in error ? error.name : undefined;
+    if (name === 'AbortError') {
+        return true;
+    }
+
+    const code = typeof error === 'object' && error !== null && 'code' in error ? error.code : undefined;
+    if (code === 20) { // DOMException.ABORT_ERR legacy code
+        return true;
+    }
+
+    const message = typeof error === 'object' && error !== null && 'message' in error
+        ? error.message
+        : typeof error === 'string'
+            ? error
+            : undefined;
+
+    if (typeof message === 'string') {
+        return INTERRUPTED_MESSAGES.some(fragment => message.includes(fragment));
+    }
+
+    return false;
+}
